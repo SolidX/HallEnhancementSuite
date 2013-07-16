f5a92606d4f36628cff352d0e4ed32bbc8264aea
SolidX <adminx@leetneet.com>
2013-07-15 20:20:33 -0400

    Debouncing added to scroll event.

--- hes.js
+++ hes.js
@@ -28,10 +28,20 @@ $.fn.isOnScreen = function(){
 	
 };
 
+/*
+ * jQuery throttle / debounce - v1.1 - 3/7/2010
+ * http://benalman.com/projects/jquery-throttle-debounce-plugin/
+ * 
+ * Copyright (c) 2010 "Cowboy" Ben Alman
+ * Dual licensed under the MIT and GPL licenses.
+ * http://benalman.com/about/license/
+ */
+(function(b,c){var $=b.jQuery||b.Cowboy||(b.Cowboy={}),a;$.throttle=a=function(e,f,j,i){var h,d=0;if(typeof f!=="boolean"){i=j;j=f;f=c}function g(){var o=this,m=+new Date()-d,n=arguments;function l(){d=+new Date();j.apply(o,n)}function k(){h=c}if(i&&!h){l()}h&&clearTimeout(h);if(i===c&&m>e){l()}else{if(f!==true){h=setTimeout(i?k:l,i===c?e-m:e)}}}if($.guid){g.guid=j.guid=j.guid||$.guid++}return g};$.debounce=function(d,e,f){return f===c?a(d,e,false):a(d,f,e!==false)}})(this);
+
 /*Initialize HES*/
 //Only Enhance visible messages in viewport
 lazyEnhanceMessages();
-$("div.hall-listview-viewport").scroll(function(){ lazyEnhanceMessages(); }); //TODO: Find a better selector for this
+$("div.hall-listview-viewport").scroll($.debounce(250, function(){ lazyEnhanceMessages(); })); //TODO: Find a better selector for this
 
 //Add SFW Mode to all open rooms
 $("div.navbar>ul.nav.btn-group.pull-right").each(function() { addSFWModeButton($(this)); });
