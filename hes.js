ccc5ad4e1fe47ed68a669496b70318f1fa3c875b
SolidX <adminx@leetneet.com>
2013-07-17 00:28:52 -0400

    HES v0.61 — SFW Button definitively fixed finally! Damn those dynamic elements not being listened to by pre-existing event listeners. Man this is a really long commit message.
    (cherry picked from commit 5ffe75dfd2e4a19d1dee54937dc57cdeaf25aacc)
    
    Conflicts:
    	hes.js

--- hes.js
+++ hes.js
@@ -3,7 +3,7 @@
 // @namespace   http://leetnet.com
 // @description Various new features for Hall.com.
 // @include     https://hall.com/*
-// @version     0.54
+// @version     0.61
 // @grant       none
 // ==/UserScript==
 
@@ -127,7 +127,7 @@ function enhanceHallMessage(hallLI) {
 }
 function addSFWModeButton(hallNav) {
 	if (hallNav.has("a.hes-sfw-mode").length == 0) {
-		hallNav.prepend("<li><a class='btn hes-sfw-mode' data-toggle='tipsy' original-title='Click to toggle SFW mode.'><div class='presence connected-busy'><i class='i'></i></div>SFW Mode</a></li>");
+		hallNav.prepend("<li><a class='btn hes-sfw-mode' data-toggle='tipsy' original-title='Click to toggle SFW mode.' onclick='toggleSFWMode($(this)); return false;'><div class='presence connected-busy'><i class='i'></i></div>SFW Mode</a></li>");
 	}
 }
 function lazyEnhanceMessages() {
@@ -136,6 +136,22 @@ function lazyEnhanceMessages() {
 	}).each(function() {
 		enhanceHallMessage($(this));
 	});
+}
+function toggleSFWMode(button) {
+	var indicator = button.children('div');
+	var header = button.closest('div.app-page-hd'); //Room Header
+	var body = header.next(); //Room Messages
+	body.find("li.hall-listview-li>a.image-embed").toggle();
+	body.find('li.hall-listview-li>a.video_embed').toggle();
+	
+	if (indicator.is('.connected-busy')) {
+		indicator.removeClass('connected-busy');
+		indicator.addClass('connected-available');
+	} else {
+		indicator.removeClass('connected-available');
+		indicator.addClass('connected-busy');
+	}
+	return false;
 }
 /*End new Message Event Handler*/
 
@@ -150,25 +166,6 @@ $(document).on("keypress", function(evt) {
 	}
 });
 
-//SFW mode
-$("a.hes-sfw-mode").on("click", function(evt) {
-	evt.stopPropagation();
-	var indicator = $(this).children('div');
-	var header = $(this).closest('div.app-page-hd'); //Room Header
-	var body = header.next(); //Room Messages
-	body.find("li.hall-listview-li>a.image-embed").toggle();
-	body.find('li.hall-listview-li>a.video_embed').toggle();
-	
-	if (indicator.is('.connected-busy')) {
-		indicator.removeClass('connected-busy');
-		indicator.addClass('connected-available');
-	} else {
-		indicator.removeClass('connected-available');
-		indicator.addClass('connected-busy');
-	}
-	return false;
-});
-
 //MGS Alert
 soundManager.createSound({
 	id:"hes-mgs-alert",
@@ -180,5 +177,5 @@ soundManager.createSound({
 /*End Support Functionality*/
 
 //Confirm handywork
-console.log("Loaded Hall Enhancement Suite 0.54");
+console.log("Loaded Hall Enhancement Suite 0.61");
 
